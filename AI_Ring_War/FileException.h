00bfdf4ef4df27bbf6c7b18bffaed746b76267e4
Lee WooJin <ujin0561@naver.com>
2025-02-04 16:51:34 +0900

    업로드 폴더 위치 수정

--- AI_Ring_War/FileException.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-struct FileException { //파일의 이름과 열기모드를 데이터 멤버로 갖습니다.
-	string filename;
-	bool bRead;
-	FileException(string name, bool b) : filename(name), bRead(b) {}
-};
